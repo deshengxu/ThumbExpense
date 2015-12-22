770f1e54dbf7dc4041203151034cb0d2e3b59659
xudesheng <xudesheng@gmail.com>
2015-12-21 22:56:21 -0500

    Initial setup for ThumbView

---
+++ ThumbExpense/ThumbView.swift
@@ -0,0 +1,21 @@
+//
+//  ThumbView.swift
+//  ThumbExpense
+//
+//  Created by Desheng Xu on 12/21/15.
+//  Copyright © 2015 Desheng Xu. All rights reserved.
+//
+
+import UIKit
+
+class ThumbView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
